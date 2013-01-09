54daeba534f626e08ef022182c27b375b594af84
Wagner Sartori Junior <wsartori@gmail.com>
2013-01-09 00:48:06 -0200

    Added 24aa from barthess for EEPROM

--- gui/pymavlink/teste.py
+++ gui/pymavlink/teste.py
@@ -1,11 +1,12 @@
 import sys
 import mavutil
+import time
 
 mavcon = mavutil.mavlink_connection("/dev/tty.usbserial-A8006AtT")
 
-print("Waiting for HEARTBEAT")
+#print("Waiting for HEARTBEAT")
 mavcon.wait_heartbeat()
-print("Heartbeat from APM (system %u component %u)" % (mavcon.target_system, mavcon.target_system))
+#print("Heartbeat from APM (system %u component %u)" % (mavcon.target_system, mavcon.target_system))
 
 while True:
 	try:
@@ -16,4 +17,7 @@ while True:
 		continue
 	msg_type = msg.get_type()
 	if msg_type == "RAW_IMU":
-		print "%d %d %d %d %d %d %d %d %d" % (msg.xacc, msg.yacc, msg.zacc, msg.xgyro, msg.ygyro, msg.zgyro, msg.xmag, msg.ymag, msg.zmag)
+		#print "%d %d %d %d %d %d %d %d %d" % (msg.xacc, msg.yacc, msg.zacc, msg.xgyro, msg.ygyro, msg.zgyro, msg.xmag, msg.ymag, msg.zmag)
+                print "%d 81 IMU_ACCEL_RAW %.4f %.4f %.4f" % (msg.time_usec, msg.xacc/4096.0, msg.yacc/4096.0, msg.zacc/4096.0)
+                print "%d 81 IMU_GYRO_RAW %d %d %d" % (msg.time_usec, msg.xgyro, msg.ygyro, msg.zgyro)
+                print "%d 81 IMU_MAG_RAW %d %d %d" % (msg.time_usec, msg.xmag, msg.ymag, msg.zmag)
